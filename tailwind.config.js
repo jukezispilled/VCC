f5218640d6e84a5150cd5012beb42c9b7d92d4d9
jukezispilled <whotfisjax@gmail.com>
2023-09-26 15:01:49 -0400

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
 /** @type {import('tailwindcss').Config} */
-module.exports = withMT({
+module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
