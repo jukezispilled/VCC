d380760eca12ecf1842f87854c80dd5500d2e209
jukezispilled <whotfisjax@gmail.com>
2023-09-26 14:49:45 -0400

    allahu akbar

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Carousel } from "@material-tailwind/react";
 
 export default function Projects() {
     return(
@@ -16,60 +15,10 @@ export default function Projects() {
                             <div className="pt-1 text-lg font-bold text-[#375688]">Owner:</div>
                             <div className="pt-1 text-lg font-bold text-[#375688]">Cost:</div>
                         </div>
-                        <Carousel
-                            className="rounded-xl h-[250px] md:h-[300px] md:w-[52%]"
-                            autoplay={true}
-                            loop={true}
-                            interval={1000}
-                            prevArrow={({ handlePrev }) => {}}
-                            nextArrow={({ handleNext }) => {}}
-                            navigation={({ setActiveIndex, activeIndex, length }) => (
-                                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                                  {new Array(length).fill("").map((_, i) => (
-                                    <span
-                                      key={i}
-                                      className={`block h-3 cursor-pointer rounded-2xl transition-all content-[''] ${
-                                        activeIndex === i ? "bg-white w-10" : "bg-white/60 w-5"
-                                      }`}
-                                      onClick={() => setActiveIndex(i)}
-                                    />
-                                  ))}
-                                </div>
-                            )}
-                        >
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                        </Carousel>
                     </div>
                 </div>
                 <div className="my-10">   
                     <div className="py-5 grid space-y-10 md:flex md:space-x-12 md:space-y-0 h-min">
-                        <Carousel
-                            className="rounded-xl h-[250px] md:h-[300px] md:w-[52%]"
-                            autoplay={true}
-                            loop={true}
-                            interval={900}
-                            prevArrow={({ handlePrev }) => {}}
-                            nextArrow={({ handleNext }) => {}}
-                            navigation={({ setActiveIndex, activeIndex, length }) => (
-                                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                                  {new Array(length).fill("").map((_, i) => (
-                                    <span
-                                      key={i}
-                                      className={`block h-3 cursor-pointer rounded-2xl transition-all content-[''] ${
-                                        activeIndex === i ? "bg-white w-10" : "bg-white/60 w-5"
-                                      }`}
-                                      onClick={() => setActiveIndex(i)}
-                                    />
-                                  ))}
-                                </div>
-                            )}
-                        >
-                            <img src="/aq1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                        </Carousel>
                         <div className="md:w-[48%]">
                             <div className="text-4xl font-bold mb-4 text-[#25406b]">Camillus Aqueduct</div>
                             <div className="text-[#2b4978] font-medium">The Camillus Aqueduct was a crucial part of the Erie Canal system, which was built in the early 19th century to connect the Hudson River with the Great Lakes. The original aqueduct was constructed in 1842 and served as a vital link in the transportation of goods and people across New York State.</div>
@@ -90,60 +39,10 @@ export default function Projects() {
                             <div className="pt-1 text-lg font-bold text-[#375688]">Owner:</div>
                             <div className="pt-1 text-lg font-bold text-[#375688]">Cost:</div>
                         </div>
-                        <Carousel
-                            className="rounded-xl h-[250px] md:h-[300px] md:w-[52%]"
-                            autoplay={true}
-                            loop={true}
-                            interval={1000}
-                            prevArrow={({ handlePrev }) => {}}
-                            nextArrow={({ handleNext }) => {}}
-                            navigation={({ setActiveIndex, activeIndex, length }) => (
-                                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                                  {new Array(length).fill("").map((_, i) => (
-                                    <span
-                                      key={i}
-                                      className={`block h-3 cursor-pointer rounded-2xl transition-all content-[''] ${
-                                        activeIndex === i ? "bg-white w-10" : "bg-white/60 w-5"
-                                      }`}
-                                      onClick={() => setActiveIndex(i)}
-                                    />
-                                  ))}
-                                </div>
-                            )}
-                        >
-                            <img src="/wpb.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                        </Carousel>
                     </div>
                 </div>
                 <div className="my-10">   
                     <div className="py-5 grid space-y-10 md:flex md:space-x-12 md:space-y-0 h-min">
-                        <Carousel
-                            className="rounded-xl h-[250px] md:h-[300px] md:w-[52%]"
-                            autoplay={true}
-                            loop={true}
-                            interval={900}
-                            prevArrow={({ handlePrev }) => {}}
-                            nextArrow={({ handleNext }) => {}}
-                            navigation={({ setActiveIndex, activeIndex, length }) => (
-                                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                                  {new Array(length).fill("").map((_, i) => (
-                                    <span
-                                      key={i}
-                                      className={`block h-3 cursor-pointer rounded-2xl transition-all content-[''] ${
-                                        activeIndex === i ? "bg-white w-10" : "bg-white/60 w-5"
-                                      }`}
-                                      onClick={() => setActiveIndex(i)}
-                                    />
-                                  ))}
-                                </div>
-                            )}
-                        >
-                            <img src="/jgb.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                        </Carousel>
                         <div className="md:w-[48%]">
                             <div className="text-4xl font-bold mb-4 text-[#25406b]">John Glenn Boulevard</div>
                             <div className="text-[#2b4978] font-medium">A bridge rehabilitation of Eastbound and Westbound structures over Onondaga lake outlet.</div>
@@ -164,31 +63,6 @@ export default function Projects() {
                             <div className="pt-1 text-lg font-bold text-[#375688]">Owner:</div>
                             <div className="pt-1 text-lg font-bold text-[#375688]">Cost:</div>
                         </div>
-                        <Carousel
-                            className="rounded-xl h-[250px] md:h-[300px] md:w-[52%]"
-                            autoplay={true}
-                            loop={true}
-                            interval={1000}
-                            prevArrow={({ handlePrev }) => {}}
-                            nextArrow={({ handleNext }) => {}}
-                            navigation={({ setActiveIndex, activeIndex, length }) => (
-                                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-                                  {new Array(length).fill("").map((_, i) => (
-                                    <span
-                                      key={i}
-                                      className={`block h-3 cursor-pointer rounded-2xl transition-all content-[''] ${
-                                        activeIndex === i ? "bg-white w-10" : "bg-white/60 w-5"
-                                      }`}
-                                      onClick={() => setActiveIndex(i)}
-                                    />
-                                  ))}
-                                </div>
-                            )}
-                        >
-                            <img src="/hoxie.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                            <img src="/proj1.jpg" alt="" className="bg-cover h-full w-full rounded-xl"></img>
-                        </Carousel>
                     </div>
                 </div>
             </div>
