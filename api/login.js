6e604fd7663bd7471832e5fdca174dd764f1ad01
jukezispilled <whotfisjax@gmail.com>
2023-09-26 17:59:24 -0400

    allahu akbar

--- api/login.js
+++ api/login.js
@@ -4,6 +4,18 @@ import jwt from 'jsonwebtoken';
 const secretKey = process.env.SECRET_KEY;
 const CONN_STRING = process.env.MONGODB_URI; // Replace with your MongoDB Atlas connection string
 
+// Define the User schema
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  role: String, // You can specify the appropriate data type for the 'role' field
+  // Other user properties...
+});
+
+// Register the 'users' model with Mongoose
+const User = mongoose.model('users', UserSchema);
+
 // Function to generate JWT
 function generateJWT(user) {
   const payload = {
@@ -25,17 +37,13 @@ export default async (req, res) => {
     // Connect to MongoDB Atlas using Mongoose
     await mongoose.connect(CONN_STRING);
 
-    // Get the users collection
-    const Users = mongoose.model('users');
-
     // Check if the user exists
-    const user = await Users.findOne({ email });
+    const user = await User.findOne({ email });
 
     // If user not found, return error
     if (!user) {
       console.error('User not found');
-      res.status(404).json({ message: 'User not found' });
-      return;
+      return res.status(404).json({ message: 'User not found' });
     }
 
     // Check if the password matches
@@ -44,20 +52,19 @@ export default async (req, res) => {
     // If the password doesn't match, return error
     if (!passwordMatches) {
       console.error('Wrong password');
-      res.status(401).json({ message: 'Wrong password' });
-      return;
+      return res.status(401).json({ message: 'Wrong password' });
     }
 
     // Generate JWT token
     const token = generateJWT(user);
 
     // Send the token as part of the response
-    res.status(200).json({ token });
-
-    // Close the Mongoose connection
-    await mongoose.disconnect();
+    return res.status(200).json({ token });
   } catch (error) {
     console.error('Failed to log in:', error);
-    res.status(500).json({ message: 'Failed to log in' });
+    return res.status(500).json({ message: 'Failed to log in' });
+  } finally {
+    // Close the Mongoose connection
+    await mongoose.disconnect();
   }
 };
