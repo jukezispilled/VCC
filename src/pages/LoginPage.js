e68fd891111fdf04e7dd19460e402fee48091ce1
jukezispilled <whotfisjax@gmail.com>
2023-09-27 04:05:37 -0400

    test

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -78,7 +78,17 @@ const LoginForm = ({ setAuthToken }) => {
 };
 
 const AdminDashboard = () => {
-  return <div>Admin Dashboard</div>;
+  return (
+    <div className="h-screen bg-cover bg-[#ffffff] bg-fixed pb-[10%]">
+      <div className="px-[10%] mx-[2%] lg:px-[15%] pt-[26%] md:pt-[8%]">
+        <div className="text-4xl lg:text-5xl font-bold my-7">Admin Dashboard</div>
+        <div className="xl:bg-[#eef0f7] h-min rounded-xl xl:shadow-md xl:p-10">
+          <div className="text-lg font-semibold">Files:</div>
+          <div className=""><a className="font-bold text-blue-600 hover:text-blue-700 transition ease-in duration-200" href="/VCCHealthandSafety.pdf" target="_blank" rel="noopener noreferrer">VCC Health and Safety</a></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const GeneralUserDashboard = () => {
