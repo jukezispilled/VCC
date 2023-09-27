47c76c6a12c34959e6ccac6b7c73ec9e0b302540
jukezispilled <whotfisjax@gmail.com>
2023-09-27 03:11:09 -0400

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -81,7 +81,6 @@ export default function Home() {
                                     </button>
                                 </a>
                             </div>
-                            <Contact />
                         </div>
                     </div>
                 </div>
